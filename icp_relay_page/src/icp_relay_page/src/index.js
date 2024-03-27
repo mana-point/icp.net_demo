5dd724af45bc468d53171ed3f4b712941cea748c
Phil Harvey <filharvey@gmail.com>
2024-03-27 12:03:42 -0400

    added latest code

--- icp_relay_page/src/icp_relay_page/src/index.js
+++ icp_relay_page/src/icp_relay_page/src/index.js
@@ -2,6 +2,15 @@ import { AuthClient } from "@dfinity/auth-client";
 import { HttpAgent } from "@dfinity/agent";
 import { MyStorage } from "./MyStorage";
 
+console.log ("Starting Call");
+
+var isChromium = window.chrome;
+var winNav = window.navigator;
+var vendorName = winNav.vendor;
+var isOpera = typeof window.opr !== "undefined";
+var isIEedge = winNav.userAgent.indexOf("Edg") > -1;
+var isIOSChrome = winNav.userAgent.match("CriOS");
+
 var identity = null;
 var url = new URL(window.location.href);
 var localhost = url.searchParams.get("localhost");
@@ -9,31 +18,66 @@ var storeName = "LiveStorage";
 var identityProvider = "https://identity.ic0.app/#authorize";
 if (localhost)
 {
-  identityProvider = "http://localhost:4943?canisterId=qjdve-lqaaa-aaaaa-aaaeq-cai#authorize"; // localhost
+  identityProvider = "http://rdmx6-jaaaa-aaaaa-aaadq-cai.localhost:4943/#authorize";
   storeName = "LocalhostStorage";
 }
 
-var storage = new MyStorage (storeName);
+console.log (identityProvider);
 
-document.getElementById("click").addEventListener("click", async (e) => {
-  e.preventDefault();
-  document.getElementById("click").setAttribute("disabled", true);
-  document.getElementById("click").setAttribute("hidden", "hidden");
+var storage = new _MyStorage__WEBPACK_IMPORTED_MODULE_2__.MyStorage (storeName);
+var isChrome = false;
+  
+if (isIOSChrome) {
+   // is Google Chrome on IOS
+} else if(
+  isChromium !== null &&
+  typeof isChromium !== "undefined" &&
+  vendorName === "Google Inc." &&
+  isOpera === false &&
+  isIEedge === false
+) 
+{
+  document.getElementById("click").addEventListener("click", async (e) => {
+    e.preventDefault();
+    document.getElementById("click").setAttribute("disabled", true);
+    document.getElementById("click").setAttribute("hidden", "hidden");
 
-  await GetIdentity ();
+    await GetIdentity ();
 
-  return false;
+    return false;
+  });
+  
+  isChrome = true;
+} 
+else 
+{ 
+    document.getElementById("click").setAttribute("disabled", true);
+    document.getElementById("click").setAttribute("hidden", "hidden");
+}
+  
+// open web socket
+console.log ("Open Websockets");
+  
+const webSocket = new WebSocket('ws://localhost:8080/data');
+var connected = false;
+  
+webSocket.addEventListener('open', () => {
+  console.log('WebSocket connection established.');
+  connected = true;
 });
 
-const webSocket = new WebSocket('ws://localhost:8080/data');
+webSocket.addEventListener('close', (e) => {
+  console.log("Websocket Closed");
+});
 
-webSocket.addEventListener('open', () => {
-    console.log('WebSocket connection established.');
+webSocket.addEventListener('error', (e) => {
+  console.log(e);
 });
 
 webSocket.addEventListener('message', event => {
   if (event.data == "Done")
   {
+    console.log ("Closing websocket");
     webSocket.close ();
     window.close ();
   }
@@ -43,15 +87,34 @@ function sendMessage(message) {
   webSocket.send(message);
 }
 
-export async function GetIdentity() {
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function checkConnection ()
+{
+  console.log ("checkConnection");
+  if (!connected)
+  {
+    setTimeout (checkConnection, 1000);
+  }
+  else
+  {
+    GetIdentity ();
+  }
+}
+
+if (!isChrome)
+  checkConnection ();
+
+async function GetIdentity() {
   try {
-    var authClient = await AuthClient.create({
+    console.log ("GetIdentity");
+
+    var authClient = await _dfinity_auth_client__WEBPACK_IMPORTED_MODULE_0__.AuthClient.create({
       storage: storage,
       keyType: 'Ed25519',
     });
-
-    const daysToAdd = 3;
-    const expiry = Date.now() + (daysToAdd * 86400000);
 
     const isAuth =  await authClient.isAuthenticated();
     if (isAuth) {
@@ -84,8 +147,13 @@ export async function GetIdentity() {
       // end of NFID
 */
       // II      
+      const daysToAdd = 3;
+      const expiry = Date.now() + (daysToAdd * 86400000);
+
       await authClient.login({
         onSuccess: async () => {
+          console.log ("Success");
+
           identity = await authClient.getIdentity();
           sendMessage (JSON.stringify (identity));
         },
